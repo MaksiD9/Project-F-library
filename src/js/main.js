7263bae21db59fba7aa84e7919856249eba383e0
Max <maximka5991@gmail.com>
2021-08-18 11:29:33 +0300

    metod

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
 import $ from './lib/lib';
 
 $('button').on('click', function() {
-    $(this).hide().show().toggleClass('active');
+    $('div').eq(1).toggleClass('active');
 });
+
+$('div').click(function() {
+    console.log($(this).index());
+});
+
+// console.log($('div').eq(2).find('.more'));
+// console.log($('.some').closest('.findme'));
+console.log($('.findme').siblings());
+// console.log($('button').html('Hello'));
